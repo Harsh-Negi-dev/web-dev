5c17377e987e71dc75d2e8df7abe7b835c66dacf
Harsh-Negi-dev <harshnegi2311@gmail.com>
2025-06-02 15:28:33 +0530

    js variables, data types & objects

---
+++ JS/script55.js
@@ -0,0 +1,26 @@
+console.log("this is practice 55")
+
+var a = 5;
+var b = 7;
+var c = "Harsh";
+
+console.log(a + b + 5)
+console.log(typeof a, typeof b, typeof c)
+
+let x = "Harsh Bhai";
+let y = 43;
+let z = 3.21;
+let p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r) 
+
+let o = {
+    "name": "Harsh", "lang": "python" 
+}
+console.log(o)
+o.age = "19";
+console.log(o)
+o.age = "20"
+console.log(o)
