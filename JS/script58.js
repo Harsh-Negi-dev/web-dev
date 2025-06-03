81a67e3b7c467edf2cbd9db2a6b7f5406d8f0840
Harsh-Negi-dev <harshnegi2311@gmail.com>
2025-06-03 11:52:25 +0530

    functions

---
+++ JS/script58.js
@@ -0,0 +1,20 @@
+// function nice(name) {
+//     console.log("Hey " + name + " you are nice");
+//     console.log("Hey " + name + " you are good");
+//     console.log("Hey " + name + " you are best");
+//     console.log("Hey " + name + " you are wonderful");
+// }
+
+// nice("Rohan");
+
+// function sum(a, b){
+//     return a + b;
+// }
+// console.log("sum of 2 and 3 is: ", sum(2, 3));
+
+const func1 = (x) =>{
+    console.log("I am an arrow function", x);
+}
+
+func1(51);
+func1(56);
