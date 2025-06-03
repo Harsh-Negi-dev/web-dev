68eda715f955a51c87e13aa2ad1e4d94c8bc3553
Harsh-Negi-dev <harshnegi2311@gmail.com>
2025-06-03 10:07:01 +0530

    loops

---
+++ JS/script57.js
@@ -0,0 +1,29 @@
+console.log("Loops");
+// let a = 1;
+// for (let i = 0; i < 100; i++){
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     "name": "Harsh", "lang": "python", "gender": "male"
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, ":", element);
+// }
+
+// for (const element of "Harsh") {
+//     console.log(element);
+// }
+
+// let a = 5;
+// while (a > 0){
+//     console.log(a);
+//     a--;
+// }
+
+let a = 0;
+do {
+    console.log(a);
+    a++;
+} while (a < 6);
