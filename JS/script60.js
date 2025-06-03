4499ab672eb571b0c48292ac0c73357f4d3e2c61
Harsh-Negi-dev <harshnegi2311@gmail.com>
2025-06-03 12:17:53 +0530

    string

---
+++ JS/script60.js
@@ -0,0 +1,26 @@
+console.log("strings tutorial");
+
+// let a = "Harsh"
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a.length);
+
+let real_name = "Harsh";
+let friend = "Jamil";
+console.log("His name is " + real_name + " and his friend's name is " + friend);
+console.log(`His name is ${real_name} and his friend's name is ${friend}`);
+
+let b = "Shivam";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 3));
+console.log(b.slice(1));
+console.log(b.replace("Sh", "77"));
+console.log(b.concat("Rahul"));
+
+let c = "           Good Boy          ";
+console.log(c.trim());
