ea5cfb7308447e0d8db98c60bd01322e4719c587
Harsh-Negi-dev <harshnegi2311@gmail.com>
2025-06-03 09:38:48 +0530

    Conditional Statements

---
+++ JS/script56.js
@@ -0,0 +1,13 @@
+console.log("Conditional statements");
+let age = 45;
+if (age > 18){
+    console.log("Eligible");
+}
+else{
+    console.log("Not Eligible");
+}
+
+let a = 5;
+let b = 7;
+let c = (a > b) ? (a + b) : (a - b);
+console.log(c);
