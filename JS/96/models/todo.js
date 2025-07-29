49381a1b7b9d61621e22846ee36f3d2e1764ed93
Harsh-Negi-dev <harshnegi2311@gmail.com>
2025-07-29 11:22:51 +0530

    tailwind css basic only

--- JS/96/models/todo.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const todoSchema = new mongoose.Schema({
-  title: String,
-  desc: String,
-  isDone: Boolean
-});
-export const todo = mongoose.model('todo', todoSchema);
